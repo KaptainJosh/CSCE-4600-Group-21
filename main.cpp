c21a2c44c756afc3aef9e726811cac790decaddb
Phong <PhongTran3@my.unt.edu>
2022-04-21 18:27:33 -0500

    Fixed the issue discussed earlier today, tested on csce machine

--- main.cpp
+++ main.cpp
@@ -9,10 +9,11 @@ int main(int argc, char *argv[])
     int processNum = 0, resourceNum = 0, lineNum = 1;
     std::string lineTxt;
     std::string data;
-    std::stringstream ss;
-    std::vector<std::vector<int>> matrix;
+    std::vector<int> unitsPerResource;
+    std::vector<std::vector<int> > matrix;
     std::vector<int> row;
-
+	std::vector<std::string> result;
+	
     std::ifstream file("Project-2-input-example.txt");
     if (!file)
     {
@@ -25,51 +26,65 @@ int main(int argc, char *argv[])
 
     while(std::getline(file, lineTxt)){
         //IF LINE BEGIN WITH '%' OR IS BLANK, SKIP TO NEXT LINE.
-        if (lineTxt[0] == '%' || lineTxt[0] == 0 )
+        //std::cout << lineTxt << std::endl;
+        if (lineTxt[0] == '%' || lineTxt.length() == 0)
             continue;
         else{
             if (lineTxt.find("num_processes") != std::string::npos){
-                //std::cout << lineTxt.back() << std::endl;
-                processNum = (int)lineTxt.back() - '0';
+				std::stringstream ss(lineTxt);
+				while(std::getline(ss, data, '=')){
+					result.push_back(data);
+				}
+				processNum = stoi(result[1]);
                 std::cout << "Process Num: " << processNum << std::endl;
                 lineNum++;
                 continue;
+
             }else if (lineTxt.find("num_resources") != std::string::npos){
-                resourceNum = (int)lineTxt.back() - '0';
+                result.clear();
+				std::stringstream ss(lineTxt);
+				while(std::getline(ss, data, '=')){
+					result.push_back(data);
+				}
+				resourceNum = stoi(result[1]);
                 std::cout << "Resource Num: " << resourceNum << std::endl;
                 lineNum++;
                 continue;
             }else if (lineNum == 3) {
+				//std::cout << "Line# " << lineNum << std::endl;
                 //list the number of units of each resource part.
-                std::vector<int> unitsPerResource;
-                ss << lineTxt;
+                //std::vector<int> unitsPerResource;
+				std::stringstream ss(lineTxt);
+				//std::cout << "line: " << lineTxt << std::endl;
                 int counter = 0;
+				//std::cout << "Counter: " << counter << std::endl;
                 while(std::getline(ss, data, ',') && counter < resourceNum){
                     unitsPerResource.push_back(stoi(data));
-                    std::cout << unitsPerResource[counter] << std::endl;
-                    if (counter > resourceNum)
-                        continue;
-                    else{
-                        counter++;
-                    }
+                    //std::cout << "Data: " << unitsPerResource[counter] << std::endl;
+                    counter++;
+					//std::cout << "Counter: " << counter << std::endl;
                 }
                 lineNum++;
             }else{
-                ss.clear();
-                ss << lineTxt;
+				std::stringstream ss(lineTxt);
                 matrix.push_back(row);
                 while(std::getline(ss,data, ',')){
-                    //std::cout << data << " ";
+                    //std::cout << data << std::endl;;
                     matrix.back().push_back(stoi(data));
                 }
-                //std::cout << std::endl;
             }
             //std::cout << "Line: " << lineTxt << std::endl;
         }
     }
 
+	//Testing printing units per resource
+	std::cout << "PRINTING UNITS PER RESOURCE" << std::endl;
+	for(auto i : unitsPerResource){
+		std::cout << i << std::endl;
+	}
+
     //Testing printing out matrix.
-    std::cout << "************************" << std::endl;
+    std::cout << "PRINTING MATRIX" << std::endl;
     for(int i = 0; i < matrix.size(); i++){
         for(int j = 0; j < matrix[i].size(); j++){
             std::cout << matrix[i][j] << " ";
